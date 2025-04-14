571e06944888ffad7a1c482ef83f21eab754bbac
amillanbc <amillan@bluecorela.com>
2025-04-14 17:27:44 -0500

    remove comments

--- cypress/support/commands.d.ts
+++ cypress/support/commands.d.ts
@@ -4,11 +4,6 @@
 
 declare namespace Cypress {
   interface Chainable {
-    /**
-     * Custom command to log in
-     * @param email - The user's email
-     * @param password - The user's password
-     */
     login(email: string, password: string): Chainable<void>;
     addToCart(): Chainable<void>;
   }
